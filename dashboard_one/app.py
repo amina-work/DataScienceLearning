17d293603576c88b8e4f28d93687240fa8beceac
amina-work <mina.works28@gmail.com>
2023-02-16 11:35:37 +0100

    error in reading files

--- dashboard_one/app.py
+++ dashboard_one/app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+
+
 df = pd.read_excel(
     io='/home/amina/Desktop/per/python-programs/dashboard_one/facilities.xlsx',
     engine="openpyxl",
@@ -9,4 +11,9 @@ df = pd.read_excel(
     nrows=2640,
 )
 
+with open('/home/amina/Desktop/per/python-programs/dashboard_one/facilities.xlsx', 'wb') as outFile:
+    outFile.write(df)
+    outFile.close()   # was missing this
+    with zipfile.ZipFile('/home/amina/Desktop/per/python-programs/dashboard_one/facilities.xlsx', 'r') as zip:
+        zip.extractall(destination)
 print(df)
